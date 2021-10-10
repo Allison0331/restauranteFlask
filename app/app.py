94556738194b034cf8eaee89ca3863792da092b5
Jhon Jairo Berastegui <jberastegui@uninorte.edu.co>
2021-10-10 11:34:39 -0500

    commit1

---
+++ app/app.py
@@ -0,0 +1,32 @@
+from flask import Flask , render_template , redirect , request , url_for , before_render_template, after_this_request
+import easygui as eg
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/ingresar')
+def ingresar():
+    return render_template('ingresar.html')
+
+@app.route('/registro_usuario')
+def registro_usuario():
+    return render_template('registro.html')
+
+@app.route('/iniciar_sesion' , methods = ['POST'])
+def iniciar_sesion():
+    nombreUser = request.form['nombreUser']
+    
+    return eg.msgbox(msg='Bienvenido' + nombreUser,
+          title='Control: msgbox', 
+          ok_button='Continuar')
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    

2d42cbbd073803fd1316f76aa43b4b6fb2e43db1
Jhon Jairo Berastegui <jberastegui@uninorte.edu.co>
2021-10-11 00:11:15 -0500

    commit menu 1

--- app/app.py
+++ app/app.py
@@ -7,15 +7,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('home.html')
-
+    
 
 @app.route('/ingresar')
 def ingresar():
     return render_template('ingresar.html')
 
+
 @app.route('/registro_usuario')
 def registro_usuario():
     return render_template('registro.html')
+
 
 @app.route('/iniciar_sesion' , methods = ['POST'])
 def iniciar_sesion():
@@ -25,6 +27,25 @@ def iniciar_sesion():
           title='Control: msgbox', 
           ok_button='Continuar')
     
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+
+@app.route('/menu/horneados')
+def menu_horneados():
+    return render_template('menuHorneados.html')
+
+
+@app.route('/menu/desayunos')
+def menu_desayunos():
+    return render_template('menuDesayunos.html')
+
+
+@app.route('/menu/bebidas')
+def menu_bebidas():
+    return render_template('/menuBebidas.html')
 
 
 if __name__ == '__main__':
